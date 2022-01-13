c9cd0806b623ef6295074dd7a220be46bc1f52d4
EGOR KICHUK <kichukegor1997@gmail.com>
2022-01-14 01:42:17 +0300

    fast-ref

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -10,31 +10,78 @@ const SHeader = styled.header`
   justify-content: space-between;
   align-items: center;
   background: ${(props) => props.color};
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 `
 
 const Navigation = styled.nav`
+  width: 354px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 22px;
+  letter-spacing: 0.25px;
 
+  a:hover {
+    color: #7C80E9;
+  }
+`
+
+const Contact = styled.button`
+  width: 147px;
+  height: 40px;
+  margin-left: 84px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid #7C80E9;
+  background: transparent;
+  box-sizing: border-box;
+  border-radius: 8px;
+  font-size: 13px;
+  font-weight: 600;
+  line-height: 22px;
+  letter-spacing: 0.25px;
+  color: #7C80E9;
+
+  & > div {
+    margin-left: 12px;
+  }
 `
 
 const Header = () => (
   <SHeader color="#F7F7FC">
     <Image width={173} height={50} src="/images/clim-art-logo.svg" alt="climArt logo"/>
-    <Navigation>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/Projects">
-        <a>Projects</a>
-      </Link>
-      <Link href="/Blog">
-        <a>Blog</a>
-      </Link>
-      <Link href="/Contact-us">
-        <a>Contact us</a>
-      </Link>
-    </Navigation>
+    <div>
+      <Navigation>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/projects">
+          <a>Projects</a>
+        </Link>
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
+      </Navigation>
+      <Contact>
+        <Image
+          src="/icons/svg/Mail.svg"
+          alt="mail"
+          width={16}
+          height={16}
+        />
+        <div>
+          Contact us
+        </div>
+      </Contact>
+    </div>
   </SHeader>
 )
 
---
+++ public/icons/DropIcon.js
@@ -0,0 +1,3 @@
+import { ReactComponent as Icon } from './svg/Drop.svg'
+
+export const DropIcon = () => (<Icon />)
